397860347b219d033392c611c4d172c76eeb6496
СидоренкоЕ <@401_7>
2018-05-10 17:57:58 +0600

    Add project files.

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+
+    public Transform player;
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    Vector3 playerPosition = player.position;
+	    playerPosition.z -= 10.0f;
+	    playerPosition.y += 2.0f;
+
+	    transform.position = playerPosition;
+	}
+}
---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+
+    public Rigidbody rigidbody;
+    public float speed;
+    public float SlideSpeed;
+    private bool IsRight = false;
+    private bool IsLeft = true;
+    private bool TurnCenter = true;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        rigidbody.AddForce(new Vector3(0, 0, speed * Time.deltaTime));
+
+        if ((Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A)) && !IsLeft)
+        {
+            Vector3 newPos = transform.position;
+            newPos.x -= 3.0f;
+            transform.position = newPos;
+            IsLeft = true;
+        }
+        else if ((Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D)) && !IsRight)
+        {
+            Vector3 newPos = transform.position;
+            newPos.x += 3.0f;
+            transform.position = newPos;
+            IsRight = true;
+        }
+        else if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
+        {
+            rigidbody.AddForce(0, 0, -(speed * Time.deltaTime));
+        }
+    }
+}
